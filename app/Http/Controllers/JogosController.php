5f1dd3e0d71bdba2df5b3fc5fbd1f679dc415d0e
ElMarcelFarias <marcel.leitefarias@gmail.com>
2024-03-20 11:12:59 -0300

    Feat: Finalizando Aula 7 - Laravel aprenda a trabalhar com models e listagem de registros no blade!

--- app/Http/Controllers/JogosController.php
+++ app/Http/Controllers/JogosController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Jogo;
 use Illuminate\Http\Request;
 
 class JogosController extends Controller {
 
     public function index() {
 
-        $nome = "Marcel";
-
-        return view('jogos.index', ['nome' => $nome]);
+        $jogos = Jogo::all();
+        //dd($jogos);
+        return view('jogos.index', ['jogos' => $jogos]);
         //helper do laravel que mostra uma variavel ou texto
     }
 
--- resources/views/jogos/index.blade.php
+++ resources/views/jogos/index.blade.php
@@ -4,5 +4,27 @@
 
 @section('content')
  <!-- Tudo aqui dentro vai ser renderizado lá no nosso template  -->
-    <h1>teste</h1>
+    <h1>Listagem de Jogos</h1>
+    <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Categoria</th>
+            <th scope="col">Ano de Criação</th>
+            <th scope="col">Valor</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($jogos as $jogo)
+            <tr>
+                <td>{{ $jogo->id }}</td>
+                <td>{{ $jogo->nome }}</td>
+                <td>{{ $jogo->categoria }}</td>
+                <td>{{ $jogo->ano_criacao }}</td>
+                <td>{{ $jogo->valor }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
